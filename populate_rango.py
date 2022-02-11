c0b0dd8b809b37beb838c5e1787196a296745abd
Dongni Tao <2552846t@student.gla.ac.uk>
2022-02-11 01:27:47 +0000

    Chapter8 exercise

--- populate_rango.py
+++ populate_rango.py
@@ -65,7 +65,7 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name, views, likes):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    c = Category.objects.get_or_create(name=name)[0]
     c.likes=likes
     c.views=views
     c.save()
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -120,14 +120,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Media files
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
-STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+# Media files
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
